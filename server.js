3d7a7e8e643fa8e6c5527bd1fd06873c07bd8ffe
Jorge Jimenez <jorgejimenez@Jorges-MacBook-Air-2.local>
2021-03-07 18:30:23 -0500

    twilio

--- server.js
+++ server.js
@@ -1,17 +1,49 @@
+
+
 const express = require("express")
 const helmet = require("helmet")
 const cors = require("cors")
 const usersRouter = require("./users/userRouter")
 const plantsRouter = require("./plants/plantsRouter")
+const bodyParser = require("body-parser");
 
-const server = express();
+const pino = require("express-pino-logger")();
+const client = require("twilio")(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
+
+const server = express(); 
 
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
 
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+server.use(pino);
+server.use(cors());
+
 server.use(usersRouter)
 server.use(plantsRouter)
+
+server.post("/api/messages", (req, res) => {
+  res.header("Content-Type", "application/json");
+  client.messages
+    .create({
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: req.body.to,
+      body: req.body.body,
+    })
+    .then(() => {
+      res.send(JSON.stringify({ success: true }));
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(JSON.stringify({ success: false }));
+    });
+});
 
 server.use((err, req, res, next) => {
   console.log(err);
